96c5424e43f939c9dee55805e057b355b27308cd
md-ali-0 <mohammad.98482@gmail.com>
2024-02-12 22:25:52 +0600

    user delete api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+import 'dotenv/config';
 import http from "http";
 import app from './src/app.js';
 import connectDB from "./src/db/connectDB.js";
-import 'dotenv/config'
 
 const server = http.createServer(app);
 const port = process.env.PORT || 8080;
@@ -10,7 +10,7 @@ async function main () {
     await connectDB();
     server.listen(port, () => {
         console.log(`Server Listing on ${port}`);
-      });      
+      });
 }
 
 main()
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,14 +2,19 @@ import { Router } from "express";
 import authVerify from "../api/auth/authVerify.js";
 import allUsers from "../api/users/allUsers.js";
 import checkUserRole from "../api/users/checkUserRole.js";
+import deleteUser from "../api/users/deleteUser.js";
 import userSignIn from "../api/users/userSignIn.js";
 import userSignUp from "../api/users/userSignUp.js";
 import xLogin from "../api/users/xLogin.js";
+import verifyAdmin from "../middlewares/verifyAdmin.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = Router()
-router.post('/signup', userSignUp)
+
 router.post('/login', userSignIn)
 router.post('/xlogin', xLogin)
+router.post('/add-user', verifyToken, verifyAdmin, userSignUp)
+router.delete('/delete-user', verifyToken, verifyAdmin, deleteUser)
 router.get('/check-user-role/:email', checkUserRole)
 router.post('/token-verify', authVerify)
 router.post('/users', allUsers)
