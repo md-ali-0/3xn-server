65884a2eda9e8ebbaf28bc071262d1bd18a2dc71
md-ali-0 <mohammad.98482@gmail.com>
2024-02-15 20:36:19 +0100

    numbers api chaged

--- src/api/emails/addNumbers.js
+++ src/api/emails/addNumbers.js
@@ -6,7 +6,7 @@ import { bcHashCompare } from "../../utils/bcrypt.js";
 const addNumbers = async(req, res)=>{
     const username = req.body.user
     const planePassword = req.body.user_pass
-    const dataName = req.body.data_id
+    const dataName = req.body.dataName
     try {
         const query = {
             username:username
--- src/models/Numbers.js
+++ src/models/Numbers.js
@@ -4,7 +4,7 @@ const NumbersSchema = new Schema(
     {
         'number' : {type: String, required: true, unique: true},
         'password' : {type: String, required: true,},
-        'userBy' : {type: String, required: true},
+        'dataName' : {type: String, required: true},
         'used' : { type: String, default: false},
         'createdAt': { type: Date, default: new Date()},
     },
